f187387746d9c6be0e5823fa75899b6b8f3391bb
IT20610616 - MadushankaHMA <88819680+MadushankaHMA@users.noreply.github.com>
2023-04-23 13:50:02 +0530

    Medical history management (#10)
    * Implemented admin route for medical history AB#4
    
    * Implemented doctor routes for medical history AB#5
    
    * Implement patient route for medical history AB#6
    
    ---------
    
    Co-authored-by: BawanthiGunasekara <it20606688@my.sliit.lk>

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -7,8 +7,12 @@ const {
 	deletePatientProfile,
 } = require("../controllers/patientController");
 
+const { getMedicalHistoryForEachPatient } = require("../controllers/medicalHistoryController");
+
 const { protect } = require("../middlewares/authPatientMiddleware");
 const router = express.Router();
+
+
 
 // Routes for Patient account operations
 router.route("/register").post(registerPatient);
@@ -16,6 +20,9 @@ router.route("/login").post(authPatient);
 router.route("/view").get(protect, getPatientProfile);
 router.route("/edit").put(protect, updatePatientProfile);
 router.route("/delete").delete(protect, deletePatientProfile);
+
+//Routes for patient to get medical history
+router.route("/medical_history/:id").get(protect, getMedicalHistoryForEachPatient);
 
 
 module.exports = router;
