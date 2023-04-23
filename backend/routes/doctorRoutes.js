123feb2c8e1dcae1a938510d62d4a5e43a55a363
kawyasamarasinghe <kawyasamarasinghe@gmail.com>
2023-04-23 12:16:19 +0530

    Implemented doctor route for user management AB#2

---
+++ backend/routes/doctorRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+	registerDoctor,
+	updateDoctorProfile,
+	authDoctor,
+	getDoctorProfile,
+	deleteDoctorProfile,
+} = require("../controllers/doctorController");
+const { 
+	getPatients, 
+	getPatientProfileById } = require("../controllers/patientController");
+
+const { protect } = require("../middlewares/authDoctorMiddleware");
+const router = express.Router();
+
+//Routes for Doctor account operations by doctor
+router.route("/register").post(registerDoctor);
+router.route("/login").post(authDoctor);
+router.route("/edit").put(protect, updateDoctorProfile);
+router.route("/view").get(protect, getDoctorProfile);
+router.route("/delete").delete(protect, deleteDoctorProfile);
+
+//Routes for Patient account operations by doctor
+router.route("/patients").get(protect, getPatients);
+router.route("/patient/profile/view/:_id").get(protect, getPatientProfileById);
+
+
+module.exports = router;
