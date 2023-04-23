a82d3b5f6dae4ca2fce819d7634de103851c7a43
mayumi09 <it20609276@my.sliit.lk>
2023-04-23 15:44:21 +0530

    Implemented doctor route for treatment mngt AB#7

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -8,9 +8,40 @@ const {
 } = require("../controllers/doctorController");
 const { 
 	getPatients, 
-	getPatientProfileById } = require("../controllers/patientController");
+	getPatientProfileById 
+} = require("../controllers/patientController");
 
-const { getMedicalHistory, getMedicalHistoryById } = require("../controllers/medicalHistoryController");
+const { 
+	getMedicalHistory, 
+	getMedicalHistoryById 
+} = require("../controllers/medicalHistoryController");
+
+const {
+	getBasicTreatments,
+	createBasicTreatment,
+	getBasicTreatmentById,
+	updateBasicTreatment,
+	deleteBasicTreatment,
+	getBasicTreatmentCount,
+} = require("../controllers/basicTreatmentController");
+
+const {
+	getOrthodontics,
+	createOrthodontic,
+	getOrthodonticById,
+	updateOrthodontic,
+	deleteOrthodontic,
+	getOrthodonticCount,
+} = require("../controllers/orthodonticController");
+
+const {
+	getFillings,
+	createFilling,
+	getFillingtById,
+	updateFilling,
+	deleteFilling,
+	getFillingCount,
+} = require("../controllers/fillingController");
 
 const { protect } = require("../middlewares/authDoctorMiddleware");
 const router = express.Router();
@@ -30,5 +61,34 @@ router.route("/patient/profile/view/:_id").get(protect, getPatientProfileById);
 router.route("/medical_history").get(protect, getMedicalHistory);
 router.route("/medical_history/:id").get(protect, getMedicalHistoryById);
 
+//Routes for basic treatment configuration by doctor
+router.route("/treatment/basic_treatment/get").get(protect, getBasicTreatments);
+router.route("/treatment/basic_treatment/get/report/").get(protect, getBasicTreatmentCount);
+router.route("/treatment/basic_treatment/create").post(protect, createBasicTreatment);
+router
+	.route("/treatment/basic_treatment/get/:id")
+	.get(protect, getBasicTreatmentById)
+	.put(protect, updateBasicTreatment)
+	.delete(protect, deleteBasicTreatment);
+
+//Routes for orthodontic treatment configuration by doctor
+router.route("/treatment/orthodontic/get").get(protect, getOrthodontics);
+router.route("/treatment/orthodontic/get/report").get(protect, getOrthodonticCount);
+router.route("/treatment/orthodontic/create").post(protect, createOrthodontic);
+router
+	.route("/treatment/orthodontic/get/:id")
+	.get(protect, getOrthodonticById)
+	.put(protect, updateOrthodontic)
+	.delete(protect, deleteOrthodontic);
+
+//Routes for filling treatment configuration by doctor
+router.route("/treatment/filling/get").get(protect, getFillings);
+router.route("/treatment/filling/get/report").get(protect, getFillingCount);
+router.route("/treatment/filling/create").post(protect, createFilling);
+router
+	.route("/treatment/filling/get/:id")
+	.get(protect, getFillingtById)
+	.put(protect, updateFilling)
+	.delete(protect, deleteFilling);
 
 module.exports = router;
